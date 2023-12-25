45d2cf5fef17a383de32447de674f04b9e43b408
Antti Ruotsalainen <antti.ruotsalainen@vuoma.fi>
2023-12-25 15:51:12 +0200

    Added djlinting for templates and shuffled code around a bit.

--- accounts/views/register.py
+++ accounts/views/register.py
@@ -1,7 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib import messages
-from django.template import Context, Template
 
 from accounts.models import User
 from django.contrib.auth.forms import UserCreationForm
--- app/settings.py
+++ app/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+CURRENT_DIR = Path(__file__).resolve().parent
+BASE_DIR = CURRENT_DIR.parent
 
 
 # Quick-start development settings - unsuitable for production
