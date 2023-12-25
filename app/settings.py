7b462dc40292411696b285988cf84bc8913a3672
Antti Ruotsalainen <antti.ruotsalainen@vuoma.fi>
2023-12-25 21:27:08 +0200

    added alpinejs and postgresql.

--- app/settings.py
+++ app/settings.py
@@ -85,11 +85,14 @@ WSGI_APPLICATION = "app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "quteo",
+        "USER": "admin",
+        "PASSWORD": "admin",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 AUTH_USER_MODEL = "accounts.User"
 LOGIN_REDIRECT_URL = "index"
