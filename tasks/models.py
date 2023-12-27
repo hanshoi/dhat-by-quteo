4dc1dd3d0d2b12b4c28efbbd5f42c9f0c3b0a1d2
Antti Ruotsalainen <antti.ruotsalainen@vuoma.fi>
2023-12-27 20:12:25 +0200

    List with actual data.

--- tasks/models.py
+++ tasks/models.py
@@ -6,12 +6,18 @@ from django.contrib.auth import get_user_model
 class Tag(models.Model):
     name = models.CharField(max_length=56)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=128, null=False, blank=False)
     description = models.TextField(default="", blank=True, null=False)
     assignee = models.ForeignKey(
-        get_user_model(), default=None, null=True, on_delete=models.SET_NULL
+        get_user_model(), default=None, null=True, blank=True, on_delete=models.SET_NULL
     )
-    tags = models.ManyToManyField(Tag)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self) -> str:
+        return self.title
--- tasks/views.py
+++ tasks/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from qframe.shortcuts import render_htmx
+from tasks.models import Task
 
 # Create your views here.
 
 
 @login_required
 def task_list(request):
-    return render_htmx(request, "task_list.html")
+    tasks = Task.objects.prefetch_related("tags").order_by("-created_at").all()[:30]
+    return render_htmx(request, "task_list.html", {"tasks": tasks})
