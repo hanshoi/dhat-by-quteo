29770bd39f1faacded85a1ca5078c7f2dfadb3bf
Antti Ruotsalainen <antti.ruotsalainen@vuoma.fi>
2023-12-28 15:20:15 +0200

    Added editing of the tasks.

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.task_list, name="tasks"),
-    path("add/", views.add_task, name="add")
+    path("add/", views.add_task, name="add"),
+    path("<int:task_id>/", views.edit_task, name="edit"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
 
 from qframe.shortcuts import render_htmx
@@ -13,7 +13,7 @@ def task_list(request):
     return render_htmx(request, "task_list.html", {"tasks": tasks})
 
 
-class AddTaskForm(forms.ModelForm):
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         exclude = ["created_at"]
@@ -22,12 +22,29 @@ class AddTaskForm(forms.ModelForm):
 @login_required
 def add_task(request):
     if request.method == "POST":
-        form = AddTaskForm(request.POST)
+        form = TaskForm(request.POST)
         if form.is_valid():
             task = form.save()
             response = render(request, "task_list.html#task", {"task": task})
-            response.headers["HX-Trigger"] = "refreshslideover"
+            response.headers["HX-Trigger"] = "refresh-slideover"
             response.headers["HX-Reswap"] = "afterbegin"
             response.headers["HX-Retarget"] = "#task-list"
             return response
-    return render(request, "task.html", {"form": AddTaskForm()})
+    return render(request, "add_task.html", {"form": TaskForm()})
+
+
+@login_required
+def edit_task(request, task_id: int):
+    task = get_object_or_404(Task, pk=task_id)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            task = form.save()
+            response = render(request, "task_list.html#task", {"task": task})
+            response.headers["HX-Trigger"] = "close-slideover"
+            response.headers["HX-Reswap"] = "outerHTML"
+            response.headers["HX-Retarget"] = f"#tasklist-item-{task.id}"
+            return response
+    return render(
+        request, "edit_task.html", {"form": TaskForm(instance=task), "task": task}
+    )
